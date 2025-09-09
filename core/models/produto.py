204d34de36f092dd3cc6235cdc0809ab024b5ae4
CaetanoPF <caetanopfreitas@gmail.com>
2025-09-09 16:48:05 -0300

    feat: interligando a model Marca com o a model Produto #3

--- core/models/produto.py
+++ core/models/produto.py
@@ -1,14 +1,26 @@
+# loja/models/produto.py
 from django.db import models
 from uploader.models import Image
 from .categoria import Categoria
+from .marca import Marca
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField(blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     estoque = models.PositiveIntegerField()
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="produtos")
-    
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.PROTECT,
+        related_name="produtos"
+    )
+    marca = models.ForeignKey(
+        Marca,
+        on_delete=models.SET_NULL,
+        related_name="produtos",
+        null=True,
+        blank=True
+    )
     imagem = models.ForeignKey(
         Image,
         related_name="+",
@@ -20,6 +32,7 @@ class Produto(models.Model):
 
     def __str__(self):
         return self.nome
+
     class Meta:
         verbose_name = "Produto"
         verbose_name_plural = "Produtos"
