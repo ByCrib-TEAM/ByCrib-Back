0099c85a79c8cddd8b3dfdfd04395e30d8aae375
CaetanoPF <caetanopfreitas@gmail.com>
2025-10-02 11:15:31 -0300

    fix: correção para permitir múltiplos superuser

--- app/settings.py
+++ app/settings.py
@@ -18,7 +18,7 @@ if MODE == 'DEVELOPMENT':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db_local.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 else:
