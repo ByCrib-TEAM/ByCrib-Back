00ceb04b43d6a82009268105ae842ef5a1ff84c6
CaetanoPF <caetanopfreitas@gmail.com>
2025-10-16 11:20:57 -0300

    feat: apagar ItemCarrinhoViewSet e urls redundantes

--- app/urls.py
+++ app/urls.py
@@ -12,9 +12,6 @@ from rest_framework.routers import DefaultRouter
 from core.views import UserViewSet, CategoriaViewSet, ProdutoViewSet, CarrinhoViewSet, ItemCarrinhoViewSet, MarcaViewSet
 router = DefaultRouter()
 
-carrinho_list = CarrinhoViewSet.as_view({'get': 'list'})
-adicionar_item = CarrinhoViewSet.as_view({'post': 'create_item'})
-
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
 router.register(r'produtos', ProdutoViewSet, basename='produtos')
@@ -37,7 +34,5 @@ urlpatterns = [
         name='redoc',
     ),
     # API
-    path('api/', include(router.urls)),
-    path('api/carrinho/', carrinho_list, name='carrinho'),
-    path('api/carrinho/adicionar/', adicionar_item, name='adicionar_item')
+    path('api/', include(router.urls))
 ]
