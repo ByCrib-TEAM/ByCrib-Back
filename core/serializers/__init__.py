c99de29f456e11db1f466025628c93b242e703f1
CaetanoPF <caetanopfreitas@gmail.com>
2025-11-11 16:47:04 -0300

    fix: db.sqlite e authentication.py

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,7 +1,6 @@
-from .user import UserSerializer
+from .user import UserWriteSerializer, UserReadSerializer
 from .categoria import CategoriaSerializer
 from .produto import ProdutoSerializer
-from .carrinho import CarrinhoSerializer
-from .item_carrinho import ItemCarrinhoSerializer
-from .compra import CompraSerializer
+from .carrinho import CarrinhoSerializer, ItemCarrinhoSerializer
 from .marca import MarcaSerializer
+from .compra import CompraSerializer
---
+++ core/serializers/authentication.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.exceptions import AuthenticationFailed
+from core.serializers import UserReadSerializer
+
+User = get_user_model()
+
+
+class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    Valida email+senha e retorna access, refresh e dados do usuário.
+    """
+    def validate(self, attrs):
+        email = attrs.get('email') or attrs.get('username')
+        password = attrs.get('password')
+
+        if not email or not password:
+            raise AuthenticationFailed('Email e senha são obrigatórios.')
+
+        user = authenticate(request=self.context.get('request'), username=email, password=password)
+        if user is None:
+            raise AuthenticationFailed('Credenciais inválidas.')
+
+        data = super().validate({'username': user.email, 'password': password})
+        data['user'] = UserReadSerializer(user, context=self.context).data
+        return data
